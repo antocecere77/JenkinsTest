70d29f2a289163763ae42d01657c0e54e89c19ad
antocecere <antocecere77@gmail.com>
2020-12-06 11:36:08 +0100

    first commit

---
+++ src/main/java/com/antocecere77/jenkins/JenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.antocecere77.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsApplication.class, args);
+	}
+
+}
